c436be2b45364bb6e49f9e94db128cdd74e47fec
Yetife <tifetreats21@gmail.com>
2021-11-20 19:23:43 +0100

    Ewa app

---
+++ src/main/java/africa/semicolon/ewaApp/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.ewaApp.exceptions;
+
+public class CustomerNotFoundException extends EwaAppException{
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
